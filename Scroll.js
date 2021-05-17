f6ac9f083ea7bcf39839a0344a958012ef11e667
傅继鹏 <fujipeng@ftl-express.cn>
2021-05-17 11:46:22 +0800

    feat:canvas提示框效果，探索canvas交互效果，滚动条效果进一步详细化

--- Scroll.js
+++ Scroll.js
@@ -15,12 +15,28 @@ class FScroll{
         this.slider.style.height=`${sliderHeight}px`
         let sliderRatio=(this.targetElement.scrollHeight-this.targetElement.clientHeight)/(height-sliderHeight)//滚动比例
         //比例修改
+
         this.targetElement.addEventListener('mousewheel',(e)=>{
             if(this.targetElement.scrollTop/sliderRatio>height-sliderHeight)
                 return
             this.targetElement.scrollTop=this.targetElement.scrollTop-(e.deltaY*this.speed)/SCROLLSPEED
             this.slider.style.top=`${this.targetElement.scrollTop/sliderRatio}px`
         })
+        //滚轮事件添加
+        this.slider.addEventListener('mousedown',(e)=>{
+            let lastPlace=e.pageY
+            let lastsSliderScroll=this.targetElement.scrollTop/sliderRatio
+            this.slider.addEventListener('mousemove',(e)=>{
+                let distance=e.pageY-lastPlace
+                let sliderScroll=lastsSliderScroll+distance
+                this.targetElement.scrollTop=sliderScroll*sliderRatio
+                if(this.targetElement.scrollTop>=200||this.targetElement.scrollTop<=0)
+                    return
+                this.slider.style.top=`${(sliderScroll)}px`
+                console.log(this.targetElement.scrollTop)
+            })
+        })
+        //添加鼠标事件
     }
 
     setSpeed(speed){
--- createGraph.js
+++ createGraph.js
@@ -1,13 +1,48 @@
+
+
+middware={
+    ctx:null
+}
+
 function Fcharts(){
     this.ctx=null
     this.path=null
+    this.element=null
     this.addDocument=function (node)
     {
-        console.log(node)
         const ctx=document.createElement('canvas')
+        let mentionElement=document.createElement('div')
+        mentionElement.setAttribute('class','mentionArea')
         node.appendChild(ctx)
+        node.appendChild(mentionElement)
+        this.element=ctx
         this.ctx=ctx.getContext('2d')
+        middware.ctx=this.ctx
         return ctx
+    }
+    this.addHoverEvent=function ()
+    {
+        console.log(this.element.offsetLeft)
+        this.element.addEventListener('mousemove',this.inPath)
+    }
+    this.inPath=function (event)
+    {
+        const  element=document.getElementsByTagName('canvas')[0]
+        let mentionElement=document.getElementsByClassName('mentionArea')[0]
+        let positionX=event.pageX-element.offsetLeft
+        let positionY=event.pageY-element.offsetTop
+        console.log(`${positionX},${positionY}`)
+        if(positionX<50&&positionY<50)
+        {
+            console.log(true)
+            mentionElement.innerText=`(${positionX}.${positionY})`
+            mentionElement.style.left=`${event.pageX+10}px`
+            mentionElement.style.top=`${event.pageY+10}px`
+            mentionElement.style.visibility='visible'
+        }
+        else {
+            mentionElement.style.visibility='hidden'
+        }
     }
     this.addData=function (position){
       let path=new Path2D()
